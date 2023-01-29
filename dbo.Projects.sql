7be8e5cfd9c5df152dba7408cd71a1a1c5e2005d
Rohan Agarwal <85097731+mr0weird0guy@users.noreply.github.com>
2023-01-29 19:08:13 +0530

    Add files via upload

---
+++ dbo.Projects.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Projects] (
+    [PId]       CHAR(6)          NOT NULL,
+    [Title]     VARCHAR (50) NOT NULL,
+    [Deadline]  DATE         NOT NULL,
+    [People]    INT          NOT NULL,
+    [ManagerId] VARCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PId] ASC),
+    FOREIGN KEY ([ManagerId]) REFERENCES [dbo].[Employees] ([Id])
+);
+
